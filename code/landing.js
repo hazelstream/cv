cdaa775aa5d7e8c0defdd5b43367bb671363de7c
Viktor Hasselström <viksv451@student.liu.se>
2021-02-09 15:11:39 +0100

    remove initial animation since it isn't scalable and changed landing page

---
+++ code/landing.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    setTimeout(() => $("#nav").fadeIn(750), 1000);
+    setTimeout(() => $(".aboutme").fadeIn(500), 1000);
+    $("#aboutbutton").toggleClass("selected");
+})
--- code/navfade.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    setTimeout(() => $("#nav").fadeIn(750), 4000);
-})
